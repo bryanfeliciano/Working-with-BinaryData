09c041ca7e3fa3ca190a1221ca9b5fa09ec74795
bryan <bryanfeliciano892@gmail.com>
2022-04-30 12:10:15 -0500

    final version, "jpg glitcher"

--- randomIO.hs
+++
@@ -1,8 +0,0 @@
-module Random where
-
-randomReplaceByte :: BC.ByteString -> IO BC.ByteString
-randomReplaceByte bytes = do
-    let bytesLength = BC.length bytes
-    location <- randomRIO (1,bytesLength)
-    charVal <- randomRIO (0,255)
-    return (replaceByte location charVal bytes)
