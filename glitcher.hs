7c3edbcab4461707c03c6a4d1cb7d351161f3474
bryan <bryanfeliciano892@gmail.com>
2022-04-30 10:06:46 -0500

    image for glitching jpg aded

---
+++ glitcher.hs
@@ -0,0 +1,3 @@
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+
--- main.hs
+++ main.hs
@@ -1,2 +1,5 @@
-import qualified Data.ByteString as B
+-- import qualified Data.ByteString as B
+-- -- Difference between these two imports is one char8 allows you to treat bytes as chars --
+-- import qualified Data.ByteString.Char8 as BC
+-- -- Thanks to this the functionality of data.text's unpack == Bc.Unpack --
 
